0561d540a068bd02ad27c0d80398241d2ab89fd3
k2901ss004 <43126013+k2901ss004@users.noreply.github.com>
2022-08-15 20:19:09 +0900

    총알 자동으로 사라지게 만듦
    bullet함수에 invoke, destroy 추가

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    
+    void Start(){
+        Invoke("DestroyBullet", 1);
+    }
+
     void Update()
     {
         transform.Translate(Vector3.forward * 1f);
+    }
+
+    void DestroyBullet(){
+        Destroy(gameObject);
     }
 }
