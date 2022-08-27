5b0be78861dca47c800218137c3d86658af6c756
k2901ss004 <43126013+k2901ss004@users.noreply.github.com>
2022-08-27 16:56:16 +0900

    인벤토리 기능/낮과밤 날짜 구현
    1. 왼쪽의 버튼 중 윗 버튼으로 인벤토리 on/off
    2. 시간의 흐름에 따른 밝기 변화 및 밤되면 몬스터 나타남(임시라서 낮되도 안 사라짐)
    3. 밤에서 다시 낮되면 왼쪽 상단 day가 늘어남

--- Assets/Script/InventoryUI.cs
+++ Assets/Script/InventoryUI.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class InventoryUI : MonoBehaviour
 {
     public GameObject inventoryPanel;
     bool activeInventory = false;
 
+    public Button inventoryBtn;
+
     private void Start(){
         inventoryPanel.SetActive(activeInventory);
+        inventoryBtn.onClick.AddListener(Inventory);
     }
+
     
-    private void Update(){
-        if(Input.GetKeyDown(KeyCode.I)){
-            activeInventory = !activeInventory;
-            inventoryPanel.SetActive(activeInventory);
-        }
+
+    void Inventory(){
+        activeInventory = !activeInventory;
+        inventoryPanel.SetActive(activeInventory);
     }
 }
