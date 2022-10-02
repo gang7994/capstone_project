78949e1b9acec03e4725f918c907dd930916a1d7
k2901ss004 <43126013+k2901ss004@users.noreply.github.com>
2022-10-02 20:38:55 +0900

    버튼 이미지 변경 및 타이머 추가
    제목 그대로

--- Assets/Script/Inventory.cs
+++ Assets/Script/Inventory.cs
@@ -4,7 +4,27 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    #region  Singleton
+    public static Inventory instance;
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this; 
+    } 
+    #endregion
     // Start is called before the first frame update
+
+    public int slotCnt
+    {
+        get => slotCnt;
+        set{
+            slotCnt = value;
+        }
+    }
     void Start()
     {
         
