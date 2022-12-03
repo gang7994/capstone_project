e49c7ac2a86ae470584b3abbd85a50f2e546f4b1
Gangyeong Lee <gang7994@naver.com>
2022-12-04 00:21:36 +0900

    인터페이스 업데이트
    PlayerMove.cs 추가

---
+++ Assets/Script/Player/PlayerMove.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 5.0f;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = CrossPlatformInputManager.GetAxisRaw("Horizontal");
+        float v = CrossPlatformInputManager.GetAxisRaw("Vertical");
+
+        if (h != 0.0f || v != 0.0f)
+        {
+            Vector3 dir = h * Vector3.right + v * Vector3.forward;
+            //transform.rotation = Quaternion.LookRotation(dir);
+            transform.Translate(dir * speed * Time.deltaTime);
+
+            //GetComponent<Animator>().SetBool("isMove", true);
+        }
+        else
+        {
+            //GetComponent<Animator>().SetBool("isMove", false);
+        }
+        
+    }
+}
