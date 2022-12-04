fd1e80ec10695f348edb85ace74611b367ccc0c6
Gangyeong Lee <gang7994@naver.com>
2022-12-04 17:59:41 +0900

    타워, 펜스 배치

--- Assets/Script/SelectTech.cs
+++ Assets/Script/SelectTech.cs
@@ -6,21 +6,27 @@ public class SelectTech : MonoBehaviour
 {
     public GameObject TechPanel;
     // Start is called before the first frame update
+    public GameObject Script;
     
 
     public void OnClick1(){
         Debug.Log("특성1 선택됨");
         TechPanel.SetActive(false);
+        Script.GetComponent<InventoryUI>().activeInventory = false;
     }
 
     public void OnClick2(){
         Debug.Log("특성2 선택됨");
         TechPanel.SetActive(false);
+        Script.GetComponent<InventoryUI>().activeInventory = false;
+
 
     }
 
     public void OnClick3(){
         Debug.Log("특성3 선택됨");
         TechPanel.SetActive(false);
+        Script.GetComponent<InventoryUI>().activeInventory = false;
+
     }
 }
