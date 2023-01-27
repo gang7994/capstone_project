870f75ed25fbe11e34f19ee575b5121d00f7fbd2
Gangyeong Lee <gang7994@naver.com>
2023-01-27 23:21:18 +0900

    ReCommit

--- Assets/Script/Build/TowerShoot.cs
+++ Assets/Script/Build/TowerShoot.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class TowerShoot : MonoBehaviour
 {
-    public Material[] mat = new Material[5]; 
+    
+
    
     private void OnTriggerEnter(Collider collision)
     {
@@ -23,22 +24,4 @@ public class TowerShoot : MonoBehaviour
         Destroy(gameObject);
         Debug.Log("총알파괴");
     }
-
-    public void selectFire(){
-        gameObject.GetComponent<TrailRenderer>().material = mat[0];
-    }
-
-    public void selectIce(){
-        gameObject.GetComponent<TrailRenderer>().material = mat[1];
-    }
-    public void selectEarth(){
-        gameObject.GetComponent<TrailRenderer>().material = mat[2];
-    }
-    public void selectPoison(){
-        gameObject.GetComponent<TrailRenderer>().material = mat[3];
-    }
-    public void selectLightning(){
-        gameObject.GetComponent<TrailRenderer>().material = mat[4];
-    }
-    
 }
