aec441d4a9fcffd6a0616fc2666edd9bc89686c7
Gangyeong Lee <gang7994@naver.com>
2023-03-14 23:38:51 +0900

    옵션버튼 클릭시 일시정지 구현
    GameManager가 없기 때문에 임시로 UI스크립트에 구현

--- Assets/Script/UI.cs
+++ Assets/Script/UI.cs
@@ -6,6 +6,7 @@ public class UI : MonoBehaviour
 {
     public GameObject Option_Panel;
 
+
     public void Option_Button_Click()
     {
         Option_Panel.SetActive(true);
@@ -15,4 +16,12 @@ public class UI : MonoBehaviour
     {
         Option_Panel.SetActive(false);
     }
+
+    public void Paused() {
+        Time.timeScale = 0f;
+    }
+    public void Resumed() {
+        Time.timeScale = 1f;
+    }
+    
 }
