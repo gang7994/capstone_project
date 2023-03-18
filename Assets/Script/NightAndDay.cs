a709be32a1e91d1dafe373cf0ea5eab4f83d17bf
gyuni <kgm805@hanyang.ac.kr>
2023-03-18 16:21:44 +0900

    타이머 제작
    밤에만 시간이 흐르도록 구현 (200초)

--- Assets/Script/NightAndDay.cs
+++ Assets/Script/NightAndDay.cs
@@ -6,38 +6,52 @@ using UnityEngine.UI;
 public class NightAndDay : MonoBehaviour
 {
     public GameObject Daylight;
-    bool night = false;
+    public Transform Number;    // UI 텍스트
+    bool isNight = false;
     Color color;
+    public float roundTime = 200f; //밤시간
+    private int day = 1;
+    private float time;
+    public bool isStop;
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Image>().fillAmount = 0.01f;
+        GetComponent<Image>().fillAmount = 0;
+        time = 0f;
+        isStop = true;
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (!isStop){
+            time += Time.fixedDeltaTime;
+            GetComponent<Image>().fillAmount += 0.0001f; 
+            if (time >= roundTime){
+                isStop = true;
+                ChangeDay();
+            }
+        }
     }
 
     public void ChangeDay()
     {
-        
-        if (GetComponent<Image>().fillAmount < 1)
+        Debug.Log("눌림");
+        isNight = !isNight;
+        GetComponent<Image>().fillAmount = 0; 
+        if (isNight) // 밤일때
         {
-            if (night)
-            {
-                Daylight.GetComponent<Light>().color = new Color(255/255f, 244/255f, 214/255f, 255f/255f);;
-                GetComponent<Image>().fillAmount += 0.01f;
-                night = false;
-            }
-            else
-            {
-
-                Daylight.GetComponent<Light>().color = new Color(0, 0, 0, 255f);
-                night = true;
-            }
-            
+            isStop = false;
+            Daylight.GetComponent<Light>().color = new Color(0, 0, 0, 255f);
         }
+        else // 낮일때
+        {
+            isStop = true;
+            time = 0;
+            day += 1; 
+            Number.GetComponent<Text>().text = day.ToString();      
+            Daylight.GetComponent<Light>().color = new Color(255/255f, 244/255f, 214/255f, 255f/255f);;
+        }     
+        
     }
 }
