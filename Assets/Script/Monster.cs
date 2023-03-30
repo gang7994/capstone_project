b440611f4d219d1f24c234b4dd8c051a310edc29
Gangyeong Lee <gang7994@naver.com>
2023-03-30 21:52:00 +0900

    불필요한 스크립트 삭제2

---
+++ Assets/Script/Monster.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Monster : MonoBehaviour
+{
+    public int maxHealth, curHealth;
+    public Transform target;
+    public bool isChase;
+
+    Rigidbody rigid;
+    CapsuleCollider capCollider;
+    Material materi;
+    NavMeshAgent navi;
+    Animator anim;
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+        capCollider = GetComponent<CapsuleCollider>();
+        materi = GetComponentInChildren<SkinnedMeshRenderer>().material;
+        navi = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
+
+        Invoke("ChaseStart", 2);
+    }
+    void Update()
+    {
+        if (isChase)
+            navi.SetDestination(target.position);
+        if(curHealth < 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+   
+
+    IEnumerator OnDamage(Vector3 reactVec)
+    {
+        materi.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+
+        if (curHealth > 0)
+        {
+            materi.color = Color.red;
+            //�� ���� ���� ���� �ʿ�
+            reactVec = reactVec.normalized;
+            reactVec += Vector3.up;
+            rigid.AddForce(reactVec * 5, ForceMode.Impulse);
+        }
+        else
+        {
+            materi.color = Color.gray;
+            anim.SetBool("isDeath", true);
+            gameObject.layer = 10;
+            Destroy(gameObject, 3);
+        }
+        
+    }
+    public void MonsterAttack()
+    {
+
+    }
+
+    void FreezeVelocity()
+    {
+        if (isChase)
+        {
+            rigid.velocity = Vector3.zero;
+            rigid.angularVelocity = Vector3.zero;
+        }
+
+    }
+    /*
+    void FixedUpdate()
+    {
+        FreezeVelocity();
+    }
+    */
+    /*
+    void ChaseStart()
+    {
+        isChase = true;
+        anim.SetBool("isWalk", true);
+    }*/
+}
