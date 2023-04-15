6096a8d394645e055c66ecd4ca552fc71ec3683a
gyuni <kgm805@hanyang.ac.kr>
2023-04-15 22:00:25 +0900

    임시 커밋
    버그 수정을 위한 임시 커밋

--- Assets/Script/Game/MonsterSpawnManager.cs
+++ Assets/Script/Game/MonsterSpawnManager.cs
@@ -8,11 +8,15 @@ public class MonsterSpawnManager : MonoBehaviour
     public GameObject Monster; //prefab으로 받는 형태, 나중에 리스트나 표로 받아와야 함
     public bool enableSpwan = false;
     int spawnMonsterNumber; //나중엔 리스트나 표로 몬스터 수도 받아와야 함
+    int spawnOnceNumOfMonster; //한번에 스폰할 몬스터 갯수
+    float spawnTime;
     public int nowMonsterNumber = 0;
     
     void Start()
     {
         spawnMonsterNumber = 30;
+        spawnOnceNumOfMonster = 10;
+        spawnTime = 10f;
         SpawnManage();
     }
 
@@ -24,14 +28,17 @@ public class MonsterSpawnManager : MonoBehaviour
 
     private void SpawnMonster()
     {
-        Vector3 spawnPosition = new Vector3(Random.Range(-50, 50), 0.5f, Random.Range(-50, 50));
-        if (enableSpwan){
-            GameObject enemy = Instantiate(Monster, spawnPosition, Quaternion.identity);
-            nowMonsterNumber += 1;
+        for (int i = 0; i < spawnOnceNumOfMonster; i++){
+            Vector3 spawnPosition = new Vector3(Random.Range(-50, 50), 0.5f, Random.Range(-50, 50));
+            if (enableSpwan){
+                GameObject enemy = Instantiate(Monster, spawnPosition, Quaternion.identity);
+                nowMonsterNumber += 1;
+            }
         }
     }
 
     public void SpawnManage(){
-        for (int i = 0; i < spawnMonsterNumber; i++) SpawnMonster();
+        int spawnNumber = spawnMonsterNumber / spawnOnceNumOfMonster;
+        for (int i = 0; i < spawnNumber; i++) Invoke("SpawnMonster", spawnTime);
     }
 }
