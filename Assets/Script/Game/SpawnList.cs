641071ca0100cd8bb75db4dd2382bfffcfd22955
gyuni <kgm805@hanyang.ac.kr>
2023-04-18 04:10:08 +0900

    JSON을 활용한 스폰 정보 받아오기 & 몬스터 마릿수 텍스트 오류 수정
    1.몬스터 마릿수를 표현할 텍스트 게임신에 추가
    2.JSON을 이용해 라운드별 몬스터 스폰 정보 초기 구현

---
+++ Assets/Script/Game/SpawnList.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+class MonsterData {
+    public string name;
+    public List<int> round;
+    public List<int> spawnMonsterNumber;
+    public List<int> spawnOnceNumOfMonster;
+}
+class SpawnList : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        JsonLoad();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void JsonLoad()
+    {
+        var loadedJson = Resources.Load<TextAsset>("roundSpawn");
+        MonsterData data = JsonUtility.FromJson<MonsterData>(loadedJson.ToString());
+        Debug.Log($"{data.name}, {data.round[1]}, {data.spawnMonsterNumber[0]}");
+    }
+}
