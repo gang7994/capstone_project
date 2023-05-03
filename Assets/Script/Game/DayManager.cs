2d536270aad1565a264d9ff9dbb27f49ec6fbddd
Gangyeong Lee <gang7994@naver.com>
2023-05-03 21:39:39 +0900

    밤 플레이중 남은 시간 UI표시

--- Assets/Script/Game/DayManager.cs
+++ Assets/Script/Game/DayManager.cs
@@ -6,12 +6,19 @@ using UnityEngine.UI;
 public class DayManager : MonoBehaviour
 {
     public GameObject Daylight;
-    public Transform Number;    // UI 텍스트
+    public GameObject day_text; 
+    public GameObject Number; //낮 UI 텍스트
+    public GameObject night_text;
+    public GameObject night_Number; //남은 시간 UI 텍스트
+
     GameManager gameManager;
     private bool isNight = false;
     Color color;
+    /* Left Time */
     /* Night Round Time */
-    float roundTime = 100f; 
+    float roundTime = 100f;
+    /* Left Time */
+    float leftTime; 
     /* Fixed Update Period, Unity Project Settings > Time > Fixed Timestep value */
     float fixedUpdateTime = 0.02f; 
     /* 
@@ -31,6 +38,8 @@ public class DayManager : MonoBehaviour
     {
         gameManager = GameObject.Find("Main Camera").GetComponent<GameManager>();
         GetComponent<Image>().fillAmount = 0;
+        night_text.SetActive(false);
+        night_Number.SetActive(false);
     }
 
     // Update is called once per frame
@@ -38,6 +47,8 @@ public class DayManager : MonoBehaviour
     {
         if (!isStop){
             time += Time.fixedDeltaTime;
+            leftTime -= Time.fixedDeltaTime;
+            if(GetComponent<Image>().fillAmount!=0) night_Number.GetComponent<Text>().text = $"{(int)leftTime}초";
             GetComponent<Image>().fillAmount += (float)fillValue; 
             if (isClear) {
                 isStop = true;
@@ -61,15 +72,27 @@ public class DayManager : MonoBehaviour
         GetComponent<Image>().fillAmount = 0; 
         if (isNight) // 밤일때
         {
+            leftTime = roundTime;
             isStop = false;
             Daylight.GetComponent<Light>().color = new Color(0, 0, 0, 255f);
+            night_text.SetActive(true);
+            night_Number.SetActive(true);
+            
+            Number.SetActive(false);
+            day_text.SetActive(false);
         }
         else // 낮일때
         {
             day += 1; 
             Number.GetComponent<Text>().text = day.ToString();      
-            Daylight.GetComponent<Light>().color = new Color(255/255f, 244/255f, 214/255f, 255f/255f);;
+            Daylight.GetComponent<Light>().color = new Color(255/255f, 244/255f, 214/255f, 255f/255f);
+            night_text.SetActive(false);
+            night_Number.SetActive(false);
+            
+            Number.SetActive(true);
+            day_text.SetActive(true);
         }     
+        
     }
 
     private void SetDayOrNight()
