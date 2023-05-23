1fafa831debf8fb42b3c6da73b17b310829136b9
Gangyeong Lee <gang7994@naver.com>
2023-05-23 23:06:03 +0900

    테크관리 패널 UI 변경

--- Assets/Script/Tech/Tech_View.cs
+++ Assets/Script/Tech/Tech_View.cs
@@ -12,6 +12,8 @@ public class Tech_View : MonoBehaviour
     public Text tech_name, tech_text;
     List<List<string>> select_tech_list;
 
+    public Sprite[] sprite;
+
     Button tower_name0, tower_name1, tower_name2, tower_name3, tower_name4, tower_name5, tower_name6, tower_name7, tower_name8, tower_name9, tower_name10,
         tower_name11, tower_name12, tower_name13, tower_name14, tower_name15;
     Text tower_text0, tower_text1, tower_text2, tower_text3, tower_text4, tower_text5, tower_text6, tower_text7, tower_text8, tower_text9, tower_text10, tower_text11,
@@ -56,26 +58,40 @@ public class Tech_View : MonoBehaviour
 
         select_tech_list = GameObject.Find("TechUI").GetComponent<Tech_Manager>().select_tech_list;
 
-        tower_name_1.AddRange(new List<Button> {tower_name0, tower_name1, tower_name2, tower_name3, tower_name4, tower_name5, tower_name6, tower_name7});
-        tower_text_1.AddRange(new List<Text> {tower_text0, tower_text1, tower_text2, tower_text3, tower_text4, tower_text5, tower_text6, tower_text7});
-        tower_name_2.AddRange(new List<Button> {tower_name8, tower_name9, tower_name10, tower_name11});
-        tower_text_2.AddRange(new List<Text> {tower_text8, tower_text9, tower_text10, tower_text11});
-        tower_name_3.AddRange(new List<Button> {tower_name12, tower_name13, tower_name14, tower_name15});
-        tower_text_3.AddRange(new List<Text> {tower_text12, tower_text13, tower_text14, tower_text15});
+       
 
-        public_name_1.AddRange(new List<Button> {public_name0, public_name1, public_name2, public_name3, public_name4, public_name5, public_name6, public_name7, public_name8, public_name9, public_name10,
-                            public_name11});
-        public_text_1.AddRange(new List<Text> {public_text0, public_text1, public_text2, public_text3, public_text4, public_text5, public_text6, public_text7, public_text8, public_text9, public_text10, public_text11});
-        public_name_2.AddRange(new List<Button> {public_name12, public_name13, public_name14, public_name15, public_name16, public_name17,
-                            public_name18, public_name19});
-        public_text_2.AddRange(new List<Text> {public_text12, public_text13, public_text14, public_text15, public_text16, public_text17, public_text18, public_text19});
-        
-        weapon_name_1.AddRange(new List<Button> { weapon_name0, weapon_name1, weapon_name2, weapon_name3, weapon_name4, weapon_name5, weapon_name6, weapon_name7});
-        weapon_text_1.AddRange(new List<Text> {weapon_text0, weapon_text1, weapon_text2, weapon_text3, weapon_text4, weapon_text5, weapon_text6, weapon_text7});
-        weapon_name_2.AddRange(new List<Button> {weapon_name8, weapon_name9, weapon_name10, weapon_name11});
-        weapon_text_2.AddRange(new List<Text> {weapon_text8, weapon_text9, weapon_text10, weapon_text11});
-        weapon_name_3.AddRange(new List<Button> {weapon_name12, weapon_name13});
-        weapon_text_3.AddRange(new List<Text> {weapon_text12, weapon_text13});
+        foreach (Button tn1 in tower_name_1)
+        {
+            tn1.GetComponent<Image>().sprite = sprite[0];
+        }
+        foreach (Button tn2 in tower_name_2)
+        {
+            tn2.GetComponent<Image>().sprite = sprite[0];
+        }
+        foreach (Button tn3 in tower_name_3)
+        {
+            tn3.GetComponent<Image>().sprite = sprite[0];
+        }
+        foreach (Button pn1 in public_name_1)
+        {
+            pn1.GetComponent<Image>().sprite = sprite[0];
+        }
+        foreach (Button pn2 in public_name_2)
+        {
+            pn2.GetComponent<Image>().sprite = sprite[0];
+        }
+        foreach (Button wn1 in weapon_name_1)
+        {
+            wn1.GetComponent<Image>().sprite = sprite[0];
+        }
+        foreach (Button wn2 in weapon_name_2)
+        {
+            wn2.GetComponent<Image>().sprite = sprite[0];
+        }
+        foreach (Button wn3 in weapon_name_3)
+        {
+            wn3.GetComponent<Image>().sprite = sprite[0];
+        }
     }
 
 
@@ -119,20 +135,23 @@ public class Tech_View : MonoBehaviour
             if(select_tech_list[i][1] == "T"){ //종류
                 if(select_tech_list[i][0] == "1") {
                     if(!t_list.Contains(select_tech_list[i][4])){
-                    tower_text_1[t1].GetComponent<Text>().text = select_tech_list[i][4];
-                    t1 += 1;
+                        tower_name_1[t1].GetComponent<Image>().sprite = sprite[1];
+                        tower_text_1[t1].GetComponent<Text>().text = select_tech_list[i][4];
+                        t1 += 1;
                     }
                 }
                 else if(select_tech_list[i][0] == "2"){
                     if(!t_list.Contains(select_tech_list[i][4])){
-                    tower_text_2[t2].GetComponent<Text>().text = select_tech_list[i][4];
-                    t2 += 1;
+                        tower_name_2[t2].GetComponent<Image>().sprite = sprite[1];
+                        tower_text_2[t2].GetComponent<Text>().text = select_tech_list[i][4];
+                        t2 += 1;
                     }
                 }
                 else if(select_tech_list[i][0] == "3"){
                     if(!t_list.Contains(select_tech_list[i][4])){
-                    tower_text_3[t3].GetComponent<Text>().text = select_tech_list[i][4];
-                    t3 += 1;
+                        tower_name_3[t3].GetComponent<Image>().sprite = sprite[1];
+                        tower_text_3[t3].GetComponent<Text>().text = select_tech_list[i][4];
+                        t3 += 1;
                     }
                 }
                 t_list.Add(select_tech_list[i][4]);
@@ -140,20 +159,23 @@ public class Tech_View : MonoBehaviour
             else if (select_tech_list[i][1] == "W"){
                 if(select_tech_list[i][0] == "1") {
                     if(!w_list.Contains(select_tech_list[i][4])){
-                    weapon_text_1[w1].GetComponent<Text>().text = select_tech_list[i][4];
-                    w1 += 1;
+                        weapon_name_1[w1].GetComponent<Image>().sprite = sprite[1];
+                        weapon_text_1[w1].GetComponent<Text>().text = select_tech_list[i][4];
+                        w1 += 1;
                     }
                 }
                 else if(select_tech_list[i][0] == "2"){
                     if(!w_list.Contains(select_tech_list[i][4])){
-                    weapon_text_2[w2].GetComponent<Text>().text = select_tech_list[i][4];
-                    w2 += 1;
+                        weapon_name_2[w2].GetComponent<Image>().sprite = sprite[1];
+                        weapon_text_2[w2].GetComponent<Text>().text = select_tech_list[i][4];
+                        w2 += 1;
                     }
                 }
                 else if(select_tech_list[i][0] == "3"){
                     if(!w_list.Contains(select_tech_list[i][4])){
-                    weapon_text_3[w3].GetComponent<Text>().text = select_tech_list[i][4];
-                    w3 += 1;
+                        weapon_name_3[w3].GetComponent<Image>().sprite = sprite[1];
+                        weapon_text_3[w3].GetComponent<Text>().text = select_tech_list[i][4];
+                        w3 += 1;
                     }
                 }
                 w_list.Add(select_tech_list[i][4]);
@@ -161,14 +183,16 @@ public class Tech_View : MonoBehaviour
             else if (select_tech_list[i][1] == "P"){
                 if(select_tech_list[i][0] == "1") {
                     if(!p_list.Contains(select_tech_list[i][4])){
-                    public_text_1[p1].GetComponent<Text>().text = select_tech_list[i][4];
-                    p1 += 1;
+                        public_name_1[p1].GetComponent<Image>().sprite = sprite[1];
+                        public_text_1[p1].GetComponent<Text>().text = select_tech_list[i][4];
+                        p1 += 1;
                     }
                 }
                 else if(select_tech_list[i][0] == "2"){
                     if(!p_list.Contains(select_tech_list[i][4])){
-                    public_text_2[p2].GetComponent<Text>().text = select_tech_list[i][4];
-                    p2 += 1;
+                        public_name_2[p2].GetComponent<Image>().sprite = sprite[1];
+                        public_text_2[p2].GetComponent<Text>().text = select_tech_list[i][4];
+                        p2 += 1;
                     }
                 }
                 
@@ -191,7 +215,7 @@ public class Tech_View : MonoBehaviour
                 if (i >= 12)
                 {
                     i -= 12;
-                    if (tower_text_3[i].GetComponent<Text>().text != ".")
+                    if (tower_text_3[i].GetComponent<Text>().text != "")
                     {
                         tech_image.sprite = Resources.Load<Sprite>(tower_text_3[i].GetComponent<Text>().text);
                         tech_name.text = tower_text_3[i].GetComponent<Text>().text;
@@ -202,7 +226,7 @@ public class Tech_View : MonoBehaviour
                 else if (i >= 8 && i <= 11)
                 {
                     i -= 8;
-                    if (tower_text_2[i].GetComponent<Text>().text != ".")
+                    if (tower_text_2[i].GetComponent<Text>().text != "")
                     {
                         tech_image.sprite = Resources.Load<Sprite>(tower_text_2[i].GetComponent<Text>().text);
                         tech_name.text = tower_text_2[i].GetComponent<Text>().text;
@@ -211,7 +235,7 @@ public class Tech_View : MonoBehaviour
                     else card_info.SetActive(false);
                 }
                 else {
-                    if (tower_text_1[i].GetComponent<Text>().text != ".")
+                    if (tower_text_1[i].GetComponent<Text>().text != "")
                     {
                         tech_image.sprite = Resources.Load<Sprite>(tower_text_1[i].GetComponent<Text>().text);
                         tech_name.text = tower_text_1[i].GetComponent<Text>().text;
@@ -239,7 +263,7 @@ public class Tech_View : MonoBehaviour
                 if (i >= 12)
                 {
                     i -= 16;
-                    if (public_text_2[i].GetComponent<Text>().text != ".")
+                    if (public_text_2[i].GetComponent<Text>().text != "")
                     {
                         tech_image.sprite = Resources.Load<Sprite>(public_text_2[i].GetComponent<Text>().text);
                         tech_name.text = public_text_2[i].GetComponent<Text>().text;
@@ -249,7 +273,7 @@ public class Tech_View : MonoBehaviour
                 }
                 else
                 {
-                    if (public_text_1[i].GetComponent<Text>().text != ".")
+                    if (public_text_1[i].GetComponent<Text>().text != "")
                     {
                         tech_image.sprite = Resources.Load<Sprite>(public_text_1[i].GetComponent<Text>().text);
                         tech_name.text = public_text_1[i].GetComponent<Text>().text;
@@ -275,7 +299,7 @@ public class Tech_View : MonoBehaviour
                 if (i >= 12)
                 {
                     i -= 12;
-                    if (weapon_text_3[i].GetComponent<Text>().text != ".")
+                    if (weapon_text_3[i].GetComponent<Text>().text != "")
                     {
                         tech_image.sprite = Resources.Load<Sprite>(weapon_text_3[i].GetComponent<Text>().text);
                         tech_name.text = weapon_text_3[i].GetComponent<Text>().text;
@@ -286,7 +310,7 @@ public class Tech_View : MonoBehaviour
                 else if (i >= 8 && i <= 11)
                 {
                     i -= 8;
-                    if (weapon_text_2[i].GetComponent<Text>().text != ".")
+                    if (weapon_text_2[i].GetComponent<Text>().text != "")
                     {
                         tech_image.sprite = Resources.Load<Sprite>(weapon_text_2[i].GetComponent<Text>().text);
                         tech_name.text = weapon_text_2[i].GetComponent<Text>().text;
@@ -296,7 +320,7 @@ public class Tech_View : MonoBehaviour
                 }
                 else
                 {
-                    if (weapon_text_1[i].GetComponent<Text>().text != ".")
+                    if (weapon_text_1[i].GetComponent<Text>().text != "")
                     {
                         tech_image.sprite = Resources.Load<Sprite>(weapon_text_1[i].GetComponent<Text>().text);
                         tech_name.text = weapon_text_1[i].GetComponent<Text>().text;
